78fa72f5a94dce70419343c9d6134c307b51ad60
YuWang(Steven) <daaiwusheng003@gmail.com>
2022-02-17 05:55:39 +0800

    体素滤波代码完成

--- lesson one/voxel_filter.py
+++ lesson one/voxel_filter.py
@@ -3,6 +3,7 @@
 import open3d as o3d
 import os
 import numpy as np
+import random
 from pyntcloud import PyntCloud
 
 
@@ -10,11 +11,26 @@ from pyntcloud import PyntCloud
 # 输入：
 #     point_cloud：输入点云
 #     leaf_size: voxel尺寸
-def voxel_filter(point_cloud, leaf_size):
+def voxel_filter(point_cloud, leaf_size, filter_mode='centroid'):
     filtered_points = []
     # 作业3
     # 屏蔽开始
-
+    max_value = np.max(point_cloud, axis=0)
+    min_value = np.min(point_cloud, axis=0)
+    # 定义 voxel grid size
+    voxel_grid_size = leaf_size
+    # 计算voxel grid 的 dimension
+    dimension = np.ceil((max_value-min_value)/voxel_grid_size)
+    indices = (point_cloud - min_value)//voxel_grid_size  # 地板除,向下取整
+    # 假设把voxel 放到一个数组中 计算其index
+    h = indices[:, 0] + indices[:, 1]*dimension[0] + indices[:, 2]*dimension[0]*dimension[1]
+    # h 这个算法,就是相当于展开三维索引到 一个维度的索引
+    for i in np.unique(h):
+        points = point_cloud[h == i]
+        if filter_mode == 'centroid':
+            filtered_points.append(np.mean(points, axis=0))
+        else:
+            filtered_points.append(random.choice(points))  # 随机采样这个接口很方便
     # 屏蔽结束
 
     # 把点云格式改成array，并对外返回
@@ -38,7 +54,8 @@ def main():
     o3d.visualization.draw_geometries([point_cloud_o3d])  # 显示原始点云
 
     # 调用voxel滤波函数，实现滤波
-    filtered_cloud = voxel_filter(point_cloud_o3d.points, 100.0)
+    # 调整这个voxel grid size可以看到不同的采样结果
+    filtered_cloud = voxel_filter(point_cloud_raw[:, 0:3], 0.1, 'random')
     point_cloud_o3d.points = o3d.utility.Vector3dVector(filtered_cloud)
     # 显示滤波后的点云
     o3d.visualization.draw_geometries([point_cloud_o3d])
